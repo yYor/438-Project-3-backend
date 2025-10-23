8256cb53219fca9a7d6cb02d5003028e6a63c4ba
Serafiend7 <eperegoy@csumb.edu>
2025-10-23 13:01:42 -0700

    SpringBoot Start
    Started the SpringBoot application using Gradle and Java 21

---
+++ src/test/java/com/example/ShopApp/ShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShopApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
