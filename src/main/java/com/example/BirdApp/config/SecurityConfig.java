7b15ef8898810da59361908c7ebcaa1bf03600c4
Serafiend7 <eperegoy@csumb.edu>
2025-12-07 00:53:20 -0800

    Changed Config to help let it run locally

--- src/main/java/com/example/BirdApp/config/SecurityConfig.java
+++ src/main/java/com/example/BirdApp/config/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
                     user = userRepository.save(user);
 
                     String redirectUrl =
-                        "exp://10.11.116.151:8081" +
+                        "exp://192.168.0.55:8081" +
                         "?userId=" + user.getUserId() +
                         "&name=" + java.net.URLEncoder.encode(name, java.nio.charset.StandardCharsets.UTF_8) +
                         "&email=" + java.net.URLEncoder.encode(email, java.nio.charset.StandardCharsets.UTF_8) +
